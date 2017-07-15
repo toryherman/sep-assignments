2a34ab2e6fe89553a0bdc79911c222609b9eb119
Tory Herman <tory.herman12@gmail.com>
2017-07-14 21:10:26 -0500

    line solution

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,48 @@ class Line
   end
 
   def join(person)
+    # push person to end of @members array/line
+    @members << person
   end
 
   def leave(person)
+    # @members array is searched and element
+    # with value person is deleted
+    @members.delete(person)
   end
 
   def front
+    # returns person at front of line
+    @members.first
   end
 
   def middle
+    # returns person in middle of line
+    mid_index = (@members.length / 2).floor
+    @members[mid_index]
   end
 
   def back
+    # returns person at back of line
+    @members.last
   end
 
   def search(person)
+    i = index(person)
+    i ? @members[i] : nil
   end
 
   private
 
   def index(person)
+    # return index location of person
+    # return nil if does not exist
+    i = 0
+    @members.each do
+      @members[i] == person ? break : i += 1
+    end
+
+    i < @members.length ? i : nil
   end
 
 end
