cfac8875571075f764b82f7b30e7ea6757bcb36b
Tory Herman <tory.herman12@gmail.com>
2017-07-19 10:39:07 -0500

    separate chaining solution

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -1,33 +1,91 @@
 require_relative 'linked_list'
+require_relative 'node'
 
 class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
+    @m = 0
   end
 
   def []=(key, value)
+    # increment m number of items in llist
+    @m += 1
+
+    if load_factor > @max_load_factor
+      resize
+    end
+
+    i = index(key, @items.size)
+    node = Node.new(key, value)
+
+    if !@items[i]
+      @items[i] = LinkedList.new()
+    end
+
+    @items[i].add_to_tail(node)
   end
 
   def [](key)
+    i = index(key, @items.size)
+
+    current = @items[i].head
+    while current && current.key != key
+      current = current.next
+    end
+
+    if current
+      current.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    sum = 0
+    key.each_char do |x|
+      sum += x.ord
+    end
+    sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    k = (@items.size).to_f
+    quotient = @m / k
+    quotient
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    new_size = @items.size * 2
+
+    @items = Array.new(new_size)
+
+    temp.each do |x|
+      if x
+        current = x.head
+        while current
+          i = index(current.key, @items.size)
+          node = Node.new(current.key, current.value)
+
+          if !@items[i]
+            @items[i] = LinkedList.new()
+          end
+
+          @items[i].add_to_tail(node)
+          current = current.next
+        end
+      end
+    end
   end
 end
